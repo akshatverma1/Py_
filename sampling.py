249e0c472c8dcfedad6f3540633ca9498fbf65c7
akshatverma1 <akshaverma14@gmail.com>
2025-07-26 00:32:53 +0530

    Python3

--- sampling.py
+++ sampling.py
@@ -9,3 +9,11 @@ print(e)
 print(type(e))
 set1 = {"a", "b", "c"}   
 print(type(set1))
+s ="'chai'"
+print(repr(s))
+
+import random
+
+print(random.randint(1, 10))
+l1 = ['a', 'b', 'c', 'd']
+print(random.choice(l1))
